c563c020f5612ca1c9d7594822f5d4b902b4bb5c
Hamsa-111 <hamsalikhitha200@gmail.com>
2026-04-02 22:40:29 +0530

    Add files via upload

---
+++ LikedList Program1(IteratorDemo).java
@@ -0,0 +1,32 @@
+/*1.Write a Java program to iterate through all elements in a linked list starting at the specified position ( 2nd) using iterator ( hint : Iterator iteratorobj = listobj.listIterator(1))
+*/
+
+
+
+package Java;
+
+import java.util.LinkedList;
+import java.util.Iterator;
+
+public class LinkedListIteratorDemo{
+public static void main(String[] args){
+
+LinkedList<String> colors=new LinkedList<>();
+
+colors.add("Red");
+colors.add("Blue");
+colors.add("Green");
+colors.add("Yellow");
+
+System.out.println("Original LinkedList: "+colors);
+
+Iterator<String> it=colors.listIterator(1);
+
+System.out.println("Elements from 2nd position:");
+
+while(it.hasNext()){
+System.out.println(it.next());
+}
+
+}
+}
