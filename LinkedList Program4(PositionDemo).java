a4736f75266b5d41be8b37ffa4a43607f2eb4679
Hamsa-111 <hamsalikhitha200@gmail.com>
2026-04-02 23:05:22 +0530

    Add files via upload

---
+++ LinkedList Program4(PositionDemo).java
@@ -0,0 +1,25 @@
+/*4.Write a Java program to display elements and their positions in a linked list ( using l_listobj.get(p) )*/
+
+
+
+package Java;
+import java.util.LinkedList;
+
+public class PositionDemo{
+public static void main(String[] args){
+
+LinkedList<String> colors=new LinkedList<>();
+
+colors.add("Red");
+colors.add("Blue");
+colors.add("Green");
+colors.add("Yellow");
+
+System.out.println("Elements with positions:");
+
+for(int i=0;i<colors.size();i++){
+System.out.println("Position "+i+" : "+colors.get(i));
+}
+
+}
+}
